3bf495fa467cac44433294f5f3d033eb8f46652d
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-06-13 18:54:11 -0300

    Se creó la página home.html, se añadió una hoja de estilos style.css, y la URL que redirige a homr.

--- catálogo_productos/catálogo_productos/settings.py
+++ catálogo_productos/catálogo_productos/settings.py
@@ -106,7 +106,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
+RUTA_CARPETA_STATIC = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    RUTA_CARPETA_STATIC,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
