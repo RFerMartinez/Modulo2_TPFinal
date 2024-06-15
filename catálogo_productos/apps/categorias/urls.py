0ec5d545a7d9126b60b34b3009ac7e57815082fa
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-06-15 10:20:41 -0300

    Se creó la función de editar una categoría y la función de eliminar una categoría. Para eliminar una categoría se dirige a otro .html preguntando si está seguro eliminar, mostrando los productos asociados. Luego se puede eliminar o cancelar

--- catálogo_productos/apps/categorias/urls.py
+++ catálogo_productos/apps/categorias/urls.py
@@ -6,4 +6,6 @@ app_name = "categorias"
 urlpatterns = [
     path('listar-categorias/', views.ListarCategorias.as_view(), name='listar_categorias'),
     path('crear-categoria/', views.CrearCategoria.as_view(), name='crear_categoria'),
+    path('editar-categoria/<int:pk>/', views.EditarCategoria.as_view(), name='editar_categoria'),
+    path('eliminar-categoria/<int:pk>/', views.EliminarCategoria.as_view(), name='eliminar_categoria'),
 ]
--- catálogo_productos/apps/categorias/views.py
+++ catálogo_productos/apps/categorias/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-from ..productos.models import Categoria
+from ..productos.models import Categoria, Producto
 from .forms import CategoriaForm
 
 class ListarCategorias(ListView):
@@ -19,3 +19,30 @@ class CrearCategoria(CreateView):
     model = Categoria
     form_class = CategoriaForm
     success_url = reverse_lazy('categorias:listar_categorias')
+
+class EditarCategoria(UpdateView):
+    template_name = "categorias/editarCategoria.html"
+    model = Categoria
+    form_class = CategoriaForm
+    success_url = reverse_lazy('categorias:listar_categorias')
+
+class EliminarCategoria(DeleteView):
+    template_name = "categorias/eliminarCategoria.html"
+    model = Categoria
+    success_url = reverse_lazy('categorias:listar_categorias')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Obtiene los productos asociados a la categoría que se está eliminando
+        context['productos_asociados'] = Producto.objects.filter(categoria=self.object)
+        return context
+
+    def eliminar(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        # Manejar lo que sucede con los productos asociados.
+        productos_asociados = Producto.objects.filter(categoria=self.object)
+        
+        # Eliminar productos asociados
+        productos_asociados.eliminar()
+
+        return super().eliminar(request, *args, **kwargs)
